c0c1579d3e808c145e2a3f95868425101b2e8aec
= <=>
2019-03-24 03:31:54 +0200

    running frontend with proxy to api on private network

--- FMP/frontend/src/constants/index.js
+++ FMP/frontend/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://localhost:3000';
 export const ACCESS_TOKEN = 'accessToken';
 export const TOKEN_TYPE = 'Bearer';
 export const USER_NAME = 'userName';
